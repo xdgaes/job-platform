30ba74c74e2130fcd42e7473991d8f6f0148cb15
xdgaes <hadissp21@gmail.com>
2025-10-23 23:22:38 +0700

    remove navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,10 @@ import Home from "./pages/Home";
 import JobList from "./pages/JobList";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="min-h-screen w-full flex flex-col bg-gray-50">
-      {/* Navbar tetap di atas */}
-      <Navbar />
 
       {/* Wrapper untuk isi halaman */}
       <main className="flex-1 w-full">
