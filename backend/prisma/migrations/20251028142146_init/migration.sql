4aa65f64985baeff5b9164e83d4840e43676a2e6
xdgaes <hadissp21@gmail.com>
2025-10-28 22:30:54 +0800

    prisma

---
+++ backend/prisma/migrations/20251028142146_init/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `role` on the `User` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "role",
+ADD COLUMN     "currentRole" TEXT NOT NULL DEFAULT 'clipper';
